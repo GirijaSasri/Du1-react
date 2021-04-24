06917702d230cd1fd687b62b05c1edbc7a61b8b0
GirijaSasri <girijasasri@gmail.com>
2021-04-24 16:49:57 +0530

    du1 project

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+import { greeting } from "../../portfolio.js";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+        
+      </Fade>
+    </div>
+  );
+}
---
+++ src/portfolio.js
@@ -0,0 +1,534 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Girija",
+  logo_name: "Portfolio.()",
+  nickname: "Girija",
+  full_name: "Girija",
+  subTitle:
+    "Front-End Developer, Open Source Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1kDL15kWyzKqTEsA5j4eei1KLW-Zw_tfZ/view?usp=sharing",
+  mail: "mailto:girijasasri@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/girijasasri",
+  linkedin: "https://www.linkedin.com/in/sasri-girija-82717719a",
+  gmail: "girijasasri@gmail.com",
+  gitlab: "https://gitlab.com/girijasasri",
+  facebook: "https://www.facebook.com/qushionmakez2gmailcom",
+  twitter: "https://twitter.com/sasri_girija",
+  instagram: "https://www.instagram.com/sasrigirija/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Front-End Developer",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end for your web applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing Web applications using bootstrap",
+        "⚡ Creating application backend panel design",
+        "⚡ Eliciting stakeholder requirements"
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "National Institute of Business Management",
+      subtitle: "Bsc (Hons) in Computing",
+      logo_path: "logo1.png",
+      alt_name: "SETI",
+      duration: "2018 - Present",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Business Analysis, etc.",
+        "⚡ I have also completed various online courses for Web Development.",
+        "⚡ I have implemented several projects based on what I've leart under my Computing degree. ",
+      ],
+      website_link: "http://www.nibm.lk/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Industry Ready Program",
+      subtitle: "Igniter Space",
+      logo_path: "mongo.png",
+       alt_name: "Igniter Space",
+      // color_code: "#2AAFED",
+      color_code: "#47A048",
+    },
+    
+    
+    {
+      title: "Certificate in Computer Science",
+      subtitle: "Igniter Space",
+      logo_path: "iit.png",
+      
+      alt_name: "Workshop",
+      color_code: "#2AAFED",
+    },
+    
+    {
+      title: "Machine learning",
+      subtitle: "Makerspace",
+      logo_path: "flutter.png",
+     
+      alt_name: "Flutter Internation Hackathon",
+      color_code: "#2AAFED",
+    },
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Front End Developer (Intern)",
+          company: "Capital Maharaja Group IT",
+          company_url: "https://www.cmg.lk/",
+          logo_path: "valora.jpg",
+          duration: "Aug 2020 - Feb 2021",
+          location: "Office",
+          description:
+            "I worked on the Dashboard project which helps IT coordinator track the activitie while using web Application. I also worked on  streaming platform Application to develop  push notifications system.",
+          color: "#0071C5",
+        },
+        {
+          title: "Front End Developer (Intern)",
+          company: "Variety Cinema Productions",
+          company_url: "https://varietycinemaproduction.com/",
+          logo_path: "nightowls.jpg",
+          duration: "Jan 2020 - Aug 2020",
+          location: "Office",
+          description:
+            "Created Front end of website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#ee3c26",
+        }
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+  
+        {
+          title: "Business Analyst",
+          company: "Variety Cinema Productions",
+          company_url: "https://varietycinemaproduction.com/",
+          logo_path: "skillenza.png",
+          duration: "Jun 2018 - Present",
+          location: "Work from Home",
+          description: "Eliciting Customer Requirements & communicating with stakeholders.",
+          color: "#196acf",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create Web Projects css,html, and React Project. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "pexels-porapak-apichodilok-346734.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/sasri_girija",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "1",
+      name: "Spot Fine Manegment System",
+      url: "",
+      description:
+        "Design and Develop Spot Fine System for koswatta Police Station to overcome the difficulties faced by the drivers and policemen.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+   
+    {
+      id: "3",
+      name: "Travel Management System",
+      url: "",
+      description:
+        "This System provides an Interface to Tourist from different section in The Hotel. And all so Tourist can book both hotel room and taxi through the website.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    
+    
+    
+    {
+      id: "5",
+      name: "Vehicle Service Application",
+      url: "",
+      description:
+        "Designed a Vehicle Service Center management system by using google map and get an appointment to vehicle And users get an email.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        },
+       
+      ],
+    },
+    
+    {
+      id: "11",
+      name: "Appointella",
+      url: "",
+      description: "This will allow customers to reserved and make payments from anywhere anytime online.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "PHP",
+          iconifyClass: "logos-php",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
